e03bc71379a8b280db4487e1252566cfb61c6131
divyadhimaan <dhimandivya713@gmail.com>
2025-09-25 10:32:06 +0530

    Vending Machine: adding inventory methods

--- code/src/vendingMachine/vendingMachine/vendingMachine.java
+++ code/src/vendingMachine/vendingMachine/vendingMachine.java
@@ -22,6 +22,8 @@ public class vendingMachine {
 
 class VendingMachineController {
     VendingInventory inventory;
+    VendingPaymentService paymentService;
+    MachineState machineState;
 }
 
 class Product {
@@ -60,7 +62,47 @@ class VendingInventory{
         return inventoryInstance;
     }
 
-    public addStock(Product product, )
+    public void addProduct(Product product, int quantity){
+        int currentStock = productStock.getOrDefault(product, 0);
+        productStock.put(product, currentStock + quantity);
+    }
 
+    public void removeStock(Product product, int quantity){
+        int currentStock = productStock.get(product);
+        if(checkAvailability(product) || currentStock - quantity < 0) {
+            System.out.println("There was no stock for the selected item: " + product.getProductName() + ". Machine misbehaving");
+            // TODO: go to invalid state
+        }
+        productStock.put(product, currentStock - quantity);
+    }
+
+    public boolean checkAvailability(Product product){
+        int currentStock = productStock.getOrDefault(product, 0);
+        if(currentStock==0){
+            System.out.println("There is no stock for the selected item: " + product.getProductName() + ".");
+            return false;
+        }
+        return true;
+    }
+
+    public boolean checkEnoughSupply(Product product, int quantity){
+        int currentStock = productStock.getOrDefault(product, 0);
+        if(currentStock - quantity < 0){
+            System.out.println("Not Enough Stock for product: " + product.getProductName());
+            if(currentStock > 0)
+                System.out.println("Quantity available: " + currentStock);
+            return false;
+        }
+        return true;
+    }
+
+
+}
+
+class VendingPaymentService{
+
+}
+
+class MachineState {
 
 }

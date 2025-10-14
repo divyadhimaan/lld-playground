040112d30235879bd29dd9c585b564d776e4d7ad
divyadhimaan <dhimandivya713@gmail.com>
2025-10-14 23:53:18 +0530

    DIP examples attached

--- code/solidPrinciples/SingleResponsibility/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-class OrderRepository {
-    public void save(OrderAfterSRP order) {
-        // Simulating database connection and save
-        System.out.println("Connecting to database...");
-        System.out.println("Saving order " + order.getOrderId() + " to database");
-        System.out.println("Order saved successfully!");
-    }
-}
